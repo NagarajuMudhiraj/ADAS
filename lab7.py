817e1cadc89dc059a8377f3b4abf1838d6afb5d1
Bheemavarapu Nagaraju <nagaraju20507@gmail.com>
2026-09-24 15:09:03 +0530

    Add files via upload

---
+++ lab7.py
@@ -0,0 +1,34 @@
+import cv2
+image =cv2.imread("road1.jpg")
+if image is None:
+    print("Image not found!")
+    exit()
+hog=cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+rects, weights=hog.detectMultiScale(
+    image,
+    winStride=(4,4),
+    padding=(8,8),
+    scale=1.03
+)
+for (x,y,w,h), weight in zip(rects, weights):
+    cv2.rectangle(
+        image,
+        (x,y),
+        (x+w,y+h),
+        (0,255,0),
+        2
+    )
+    cv2.putText(
+        image,
+        f"Person:{float(weight):.2f}",
+        (x,y-10),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.5,
+        (0,255,0),
+        2
+    )
+print("Number of pedestrians detected:",len(rects))
+cv2.imshow("Pedestrian Detection",image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
